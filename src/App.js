ef9fb478adf45b3997046bd91b68f9c0e5d5fca8
титаренко <kizuneworld@gmail.com>
2019-02-21 16:54:07 +0200

    tic-toe

--- src/App.js
+++ src/App.js
@@ -1,28 +1,150 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class Reset extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <button
+        onClick={() => this.props.onClick()}
+      >
+        New Game
+      </button>
+    );
+  }
+}
+
+class Square extends React.Component {
+  render() {
+    const winClass = `square ${this.props.winClass}`;
+    return (
+      <button
+        className={winClass}
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      x: true,
+    };
+  }
+
+  handleClick(i) {
+    const square = this.state.squares.slice();
+    if (calculateWinner(square).winner || square[i]) {
+      return;
+    }
+    square[i] = this.state.x ? 'X' : 'O';
+    this.setState({
+      squares: square,
+      x: !this.state.x,
+    });
+  }
+
+  handleClickNewGame() {
+    this.setState(
+      {
+        squares: Array(9).fill(null),
+        x: true,
+      },
+    );
+  }
+
+  renderSquare(i, winLine) {
+    let winClass = '';
+    if (winLine) {
+      winClass = (winLine.indexOf(i) !== -1) ? 'winner_line' : '';
+    }
+    return (
+      <Square
+        winClass={winClass}
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const { winner } = calculateWinner(this.state.squares);
+    let winLine = '';
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+      winLine = calculateWinner(this.state.squares).winLine;
+    } else if (this.state.squares.indexOf(null) === -1) {
+      status = 'Try again...';
+    } else {
+      status = `Next player: ${this.state.x ? 'X' : 'O'}`;
+    }
+    return (
+      <div>
+        <div className="status">
+          {status}
+          {' '}
+        </div>
+
+        <div className="board-row">
+          {this.renderSquare(0, winLine)}
+          {this.renderSquare(1, winLine)}
+          {this.renderSquare(2, winLine)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3, winLine)}
+          {this.renderSquare(4, winLine)}
+          {this.renderSquare(5, winLine)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6, winLine)}
+          {this.renderSquare(7, winLine)}
+          {this.renderSquare(8, winLine)}
+        </div>
+        <div className="new_game_button">
+          <Reset
+            onClick={() => this.handleClickNewGame()}
+          />
+        </div>
+      </div>
+
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
       </div>
     );
   }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return {
+        winner: squares[a],
+        winLine: lines[i],
+      };
+    }
+  }
+  return {
+    winner: null,
+    winLine: null,
+  };
+}
 export default App;
